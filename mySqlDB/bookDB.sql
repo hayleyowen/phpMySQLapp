7b8c6d55925abc5f417ad545e3db7a8adcd9d2a7
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2024-10-11 14:51:50 -0500

    Update bookDB.sql according to class

--- mySqlDB/bookDB.sql
+++ mySqlDB/bookDB.sql
@@ -18,6 +18,8 @@
 --
 -- Table structure for table `book_category_relationship`
 --
+CREATE DATABASE bookstore; 
+USE bookstore; 
 
 DROP TABLE IF EXISTS `book_category_relationship`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
